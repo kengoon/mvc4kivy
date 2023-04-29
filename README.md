9362e72c6f0a0eb778be67382cfa8430fbc63f68
Kenechukwu Akubue <kengoon19@gmail.com>
2023-04-29 10:34:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mvc4kivy (m4k)
+Borrowed from the [kivymd's](https://github.com/kivymd/KivyMD) MVC tool project creator but with some features added
+like lazy loading of kv file and lazy adding of screen to improve application speed at startup.
---
+++ mvc4kivy/MVC/Model/database_firebase.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import socket
+
+from pyrebaselite import initialize_app
+
+firebase = initialize_app({})
+
+
+def get_connect(func, host="8.8.8.8", port=53, timeout=3):
+    """Checks for an active Internet connection."""
+
+    def wrapped(*args):
+        try:
+            socket.setdefaulttimeout(timeout)
+            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(
+                (host, port)
+            )
+            return func(*args)
+        except Exception:
+            return False
+
+    return wrapped
+
+
+class DataBase:
+    """
+    Your methods for working with the database should be implemented in this
+    class.
+    """
+
+    name = "Firebase"
+
+    def __init__(self):
+        self.auth = firebase.auth()
+        self.database = firebase.database()
+        self.storage = firebase.storage()
